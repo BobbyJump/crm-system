10620a50cc3b33ae78d2ccffe4853ab28a5aff8f
Dmitriy <skachok.d@gmail.com>
2019-12-02 17:15:08 +0200

    add passport

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const passport = require('passport')
 const bodyParser = require('body-parser')
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
@@ -9,12 +10,14 @@ const positionRoutes = require('./routes/position')
 const keys = require('./config/keys')
 const app = express()
 
-//connected mongo to the project
+//connect mongo to the project
 mongoose.connect(keys.mongoURL)
     .then(() => console.log('MongoDB connected.'))
     .catch(error => console.log(error))
 //-------------------------------------------------
 
+app.use(passport.initialize())
+require('./middleware/passport')(passport)
 
 app.use(require('morgan')('dev'))
 app.use(require('cors')())

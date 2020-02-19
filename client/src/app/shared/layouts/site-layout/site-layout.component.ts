73b1a3c96c404725c30a2f1a7a34a98a17572d46
Dmitriy <skachok.d@gmail.com>
2020-02-19 02:07:46 +0200

    creation of main page/site layout

--- client/src/app/shared/layouts/site-layout/site-layout.component.ts
+++ client/src/app/shared/layouts/site-layout/site-layout.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { MaterialService } from '../../classes/material.service';
 
 @Component({
   selector: 'app-site-layout',
   templateUrl: './site-layout.component.html',
   styleUrls: ['./site-layout.component.css']
 })
-export class SiteLayoutComponent implements OnInit {
 
-  constructor() { }
+export class SiteLayoutComponent implements AfterViewInit {
 
-  ngOnInit() {
+  @ViewChild("floating", {static: false}) floatingRef: ElementRef
+
+  links = [
+    {url: '/overview', name: 'Overview'},
+    {url: '/analytics', name: 'Analytics'},
+    {url: '/history', name: 'History'},
+    {url: '/order', name: 'Add order'},
+    {url: '/categories', name: 'Assortment'},
+  ]
+
+  constructor(private auth: AuthService,
+              private router: Router) {
+
+              }
+
+  ngAfterViewInit(){
+    MaterialService.initializeFloatingButton(this.floatingRef)
   }
 
+  logout(event: Event){
+    event.preventDefault()
+    this.auth.logOut()
+    this.router.navigate(['/login'])
+  }
 }

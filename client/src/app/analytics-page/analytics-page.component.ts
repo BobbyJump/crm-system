bdc993f2ad18e66427bb452ec4f1ce94aab064c6
Dmitriy <skachok.d@gmail.com>
2020-02-22 21:23:41 +0200

    drawing analytics graphs

--- client/src/app/analytics-page/analytics-page.component.ts
+++ client/src/app/analytics-page/analytics-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 import { AnalyticsService } from '../shared/services/analytics.service';
 import { AnalyticsPage } from '../shared/interfaces';
+import { Chart } from 'chart.js'
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -20,9 +21,37 @@ export class AnalyticsPageComponent implements AfterViewInit, OnDestroy {
   constructor(private service: AnalyticsService) { }
 
   ngAfterViewInit(){
+    const revenueConfig: any = {
+      label: 'Revenue',
+      color: 'rgb(255, 99, 132)'
+    }
+
+    const orderConfig: any = {
+      label: 'Orders',
+      color: 'rgb(54, 162, 235)'
+    }
+
     this.aSub = this.service.getAnalytics().subscribe((data: AnalyticsPage) => {
       this.average = data.average
+      // For revenue
+      revenueConfig.labels = data.chart.map(item => item.label)
+      revenueConfig.data = data.chart.map(item => item.revenue)
 
+      const revenueCtx = this.revenueRef.nativeElement.getContext('2d')
+
+      revenueCtx.canvas.height = '300px'
+      new Chart(revenueCtx, createChartConfig(revenueConfig))
+      
+      // For orders
+      orderConfig.labels = data.chart.map(item => item.label)
+      orderConfig.data = data.chart.map(item => item.order)
+      
+      const orderCtx = this.orderRef.nativeElement.getContext('2d')
+      
+      orderCtx.canvas.height = '300px'
+      
+      new Chart(orderCtx, createChartConfig(orderConfig))
+      
       this.pending = false
     })
   }
@@ -33,4 +62,24 @@ export class AnalyticsPageComponent implements AfterViewInit, OnDestroy {
     }
   }
 
+}
+
+function createChartConfig({labels, data, label, color}){
+  return {
+    type: 'line',
+    options: {
+      responsive: true
+    },
+    data: {
+      labels,
+      datasets: [
+        {
+          label, data,
+          borderColor: color,
+          steppedLine: false,
+          fill: false
+        }
+      ]
+    }
+  }
 }
